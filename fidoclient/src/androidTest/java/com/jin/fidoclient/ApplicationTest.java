903ca5cdff4e700c36b9c03a0fb7b13fcd77d50f
jinkg <349218364@qq.com>
2016-02-23 11:11:26 +0800

    add test

--- fidoclient/src/androidTest/java/com/jin/fidoclient/ApplicationTest.java
+++ fidoclient/src/androidTest/java/com/jin/fidoclient/ApplicationTest.java
@@ -25,23 +25,14 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testCert() {
-        try {
-            byte[] pub = KeyCodec.getKeyAsRawBytes((ECPublicKey) KeyCodec.getPubKey(Base64.decode(AttestCert.pubCert, Base64.URL_SAFE)));
-            X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(
-                    new ByteArrayInputStream(pub));
-            String certBase64 = Base64.encodeToString(cert.getEncoded(), Base64.URL_SAFE);
-            System.out.println(certBase64);
-        } catch (CertificateException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
+
+    public void testString() {
+        String msg = "[{\"challenge\":\"JDJhJDEwJGpTdXZkN0hOS0pEZS91Tm11LksxMGU\",\"header\":{\"appID\":\"https://www.head2toes.org/fidouaf/v1/public/uaf/facets\",\"op\":\"Auth\",\"serverData\":\"MTdiOGJmMTM4MDNjN2ZjZTAwNzYxYjM3MWIxODIyNjE0MDUyOGMyYzJlODRiOGFkNzI2ODc0NjIzNTlhMjE1OS5NVFExTmpFNU5UUTBPVE0zTWcuU2tSS2FFcEVSWGRLUjNCVVpGaGFhMDR3YUU5VE1IQkZXbE01TVZSdE1URk1hM040VFVkVg\",\"upv\":{\"major\":1,\"minor\":0}},\"policy\":{\"accepted\":[[{\"aaid\":[\"EBA0#0001\"]}],[{\"aaid\":[\"4e4e#4005\"]}],[{\"aaid\":[\"EEEE#0001\"]}],[{\"aaid\":[\"0012#0002\"]}],[{\"aaid\":[\"0010#0001\"]}],[{\"aaid\":[\"4e4e#0001\"]}],[{\"aaid\":[\"5143#0001\"]}],[{\"aaid\":[\"0011#0701\"]}],[{\"aaid\":[\"0013#0001\"]}],[{\"aaid\":[\"0014#0000\"]}],[{\"aaid\":[\"0014#0001\"]}],[{\"aaid\":[\"53EC#C002\"]}],[{\"aaid\":[\"DAB8#8001\"]}],[{\"aaid\":[\"DAB8#0011\"]}],[{\"aaid\":[\"DAB8#8011\"]}],[{\"aaid\":[\"5143#0111\"]}],[{\"aaid\":[\"5143#0120\"]}],[{\"aaid\":[\"4746#F816\"]}],[{\"aaid\":[\"53EC#3801\"]}]]}}]";
+        String UPV_TAG = "{\"major\": 1, \"minor\": 0}";
+        String MAJOR_TAG = "\"major\": 1";
+        String MINOR_TAG = "\"minor\": 0";
+        assert (msg.contains(UPV_TAG));
+        assert (msg.contains(MAJOR_TAG));
+        assert (msg.contains(MINOR_TAG));
     }
 }
